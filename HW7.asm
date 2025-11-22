aacaf303acd554b27fad646063ca387064f2f7f7
Koautl <163625191+Koautl@users.noreply.github.com>
2025-11-22 11:30:49 -0500

    Update HW7.asm

--- HW7.asm
+++ HW7.asm
@@ -1,4 +1,4 @@
-;Robert Martinez-Olivera
+;
 ;Date - 11/29/2023
 INCLUDE Irvine32.inc
     .data
@@ -34,6 +34,7 @@ GradeCalc PROC ; pass in array, and compare each element in the array
 .ELSEIF <> ;if element in array is  60 between 69 print D
 .ELSE <> . ; print F
 GradeCalc ENDP
+
 
 
 
